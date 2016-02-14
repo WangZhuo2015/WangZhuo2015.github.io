e885ff0d1afe58e8db076316172357977bc13bf2
王卓 <280034816@qq.com>
2016-02-15 00:35:15 +0800

    DONE
    DONE

--- functions.js
+++ functions.js
@@ -52,5 +52,5 @@ function timeElapse(date){
 		seconds = "0" + seconds;
 	}
 	var result = "爱上你的第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
-	$("#clock").html(result);
+	//$("#clock").html(result);
 }
